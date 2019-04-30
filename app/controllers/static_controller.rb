e6a0f1d36029a4b1ddffcaf52d8d00033832d161
Bryn Bennett <bryn.bennett@gmail.com>
2019-04-30 19:12:45 +0000

    Automatically backed up by Learn

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,7 @@
 class StaticController < ApplicationController
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> 2bc352529428caaa8e9ad69fc3cd01778db203ce
 end
--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,4 +1,5 @@
 class StoreAdminController < ApplicationController
+<<<<<<< HEAD
   layout "admin"
 
   def orders
@@ -8,4 +9,6 @@ class StoreAdminController < ApplicationController
   def invoice
     render :layout => nil 
   end
+=======
+>>>>>>> 2bc352529428caaa8e9ad69fc3cd01778db203ce
 end
